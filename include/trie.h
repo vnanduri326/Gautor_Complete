460b7de955de4db775c1a18574c7f7e16cf401a2
vignesh <vignesh.nanduri@gmail.com>
2026-07-08 15:00:57 -0400

    added trie node implementation header and src files

---
+++ include/trie.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <cstdint> 
+
+struct TrieNode {
+    TrieNode* children[26];
+    bool isEndOfWord;
+    uint64_t frequency;
+
+    TrieNode();
+    ~TrieNode();
+};
+
+class Trie {
+private:
+    TrieNode* root;
+    void dfsHelper(TrieNode* current, std::string currentWord, std::vector<std::pair<std::string, uint64_t>>& candidates);
+
+public:
+    Trie();
+    ~Trie();
+
+    void insert(const std::string& word, uint64_t frequency);
+    std::vector<std::string> getTopK(const std::string& prefix, int k);
+};
---
+++ src/trie.cpp
@@ -0,0 +1,64 @@
+#include "include/trie.h"
+#include <algorithm>
+TrieNode::TrieNode(){
+    isEndOfWord = false;
+    frequency = 0;
+    for(int i = 0; i < 26; ++i){
+        children[i] = nullptr;
+    }
+}
+TrieNode::~TrieNode(){
+    for(int i = 0; i < 26; ++i){
+        delete children[i];
+    }
+}
+Trie::Trie() {
+    root = new TrieNode();
+}
+Trie::~Trie() {
+    delete root;
+}
+void Trie::dfsHelper(TrieNode* current, std::string currentWord, std::vector<std::pair<std::string, uint64_t>>& candidates) {//helps find topK words
+    if (current->isEndOfWord) {
+        candidates.push_back({currentWord, current->frequency});
+    }
+    for (int i = 0; i < 26; ++i) {
+        if (current->children[i]) {
+            dfsHelper(current->children[i], currentWord + static_cast<char>('a' + i), candidates);
+        }
+    }
+}
+void Trie::insert(const std::string& word, uint64_t frequency) {
+    TrieNode* current = root;
+    for (char c : word) {
+        int index = c - 'a';
+        if (!current->children[index]) {
+            current->children[index] = new TrieNode();
+        }
+        current = current->children[index];
+    }
+    current->isEndOfWord = true;
+    current->frequency = frequency;
+}
+std::vector<std::string> Trie::getTopK(const std::string& prefix, int k) {
+    TrieNode* current = root;
+    for (char c : prefix){
+        int index = c - 'a';
+        if (!current->children[index]) {
+            return {}; //prefix not found
+        }
+        current = current->children[index];
+    }
+    std::vector<std::pair<std::string, uint64_t>> candidates;
+    dfsHelper(current, prefix, candidates);
+    std::sort(candidates.begin(), candidates.end(), [](const auto& a, const auto& b) {
+        return a.second > b.second; //sort by frequency in descending order
+    });
+    std::vector<std::string> res;
+    for (int i = 0; i < std::min(k, static_cast<int>(candidates.size())); ++i) {
+        res.push_back(candidates[i].first);
+    }
+    return res;   
+    
+
+}
